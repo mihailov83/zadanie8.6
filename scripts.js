9664ebeda970a76a92ccdc32807b719a88a287f6
mihailov83 <42243106+mihailov83@users.noreply.github.com>
2018-09-25 23:22:45 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,20 +2,19 @@
 
 var a = 4,
     b = 2,
-    value= (a * a) - (2 * a * b) - (b * b); 
+    value = (a * a) - (2 * a * b) - (b * b);
 
 
 if (value < 0) {
-		console.log("The value is below zero");
-		alert("The value is below 0");
-}	else if (value > 0) {
-		console.log("The value is above 0");
-		alert("The value is above 0");
-}	else if (value == 0) {
-		console.log("The value is 0");
-		alert("The value is 0");  
-}	else {
-		console.log("This is an unknown value");
-		alert("This is an unknown value")
+    console.log("The value is below zero");
+    alert("The value is below 0");
+} else if (value > 0) {
+    console.log("The value is above 0");
+    alert("The value is above 0");
+} else if (value == 0) {
+    console.log("The value is 0");
+    alert("The value is 0");
+} else {
+    console.log("This is an unknown value");
+    alert("This is an unknown value")
 }
-
